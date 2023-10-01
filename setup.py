3e86a8a558e288e98199783970b442f4d7cf1bce
Ouroboros Coding Inc <chris@ouroboroscoding.com>
2023-10-01 10:33:40 -0400

    Hotfix 1.2.2 - Updated version - Added keyword to project

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('README.md', 'r') as oF:
 	long_description=oF.read()
 
 setup(
-	name='Tools-OC',
-	version='1.2.1',
+	name='tools-oc',
+	version='1.2.2',
 	description='A set of tools for common python problems',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
@@ -14,7 +14,7 @@ setup(
 		'Source': 'https://github.com/ouroboroscoding/tools-python',
 		'Tracker': 'https://github.com/ouroboroscoding/tools-python/issues'
 	},
-	keywords=['tools', 'clone', 'merge', 'combine'],
+	keywords=['tools', 'clone', 'diff', 'merge', 'combine'],
 	author='Chris Nasr - Ouroboros Coding Inc.',
 	author_email='chris@ouroboroscoding.com',
 	license='MIT',
